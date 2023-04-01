077ae06367300caeabace9eae97854cc04f37c89
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-04-01 15:12:24 +0200

    Common requires "uses" for some point

--- AsteroidsGame/Common/src/main/java/module-info.java
+++ AsteroidsGame/Common/src/main/java/module-info.java
@@ -1,6 +1,16 @@
+import dk.sdu.student.mialb21.common.services.IBulletCreator;
+import dk.sdu.student.mialb21.common.services.IEntityProcessingService;
+import dk.sdu.student.mialb21.common.services.IGamePluginService;
+import dk.sdu.student.mialb21.common.services.IPostEntityProcessingService;
+
 module Common {
     exports dk.sdu.student.mialb21.common.services;
     exports dk.sdu.student.mialb21.common.data;
     exports dk.sdu.student.mialb21.common.data.entityparts;
     exports dk.sdu.student.mialb21.common.util;
+
+    uses IBulletCreator;
+    uses IGamePluginService;
+    uses IEntityProcessingService;
+    uses IPostEntityProcessingService;
 }
