9fe5418abe3739fc68b0150125c6f067f1459187
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-04-01 15:10:08 +0200

    Updated formatting

--- AsteroidsGame/Asteroid/src/main/java/module-info.java
+++ AsteroidsGame/Asteroid/src/main/java/module-info.java
@@ -3,6 +3,7 @@ import dk.sdu.student.mialb21.common.services.IGamePluginService;
 
 module Asteroid {
     requires Common;
+
     provides IGamePluginService with dk.sdu.student.mialb21.asteroid.AsteroidPlugin;
     provides IEntityProcessingService with dk.sdu.student.mialb21.asteroid.AsteroidControlSystem;
 }
--- AsteroidsGame/Collision/src/main/java/module-info.java
+++ AsteroidsGame/Collision/src/main/java/module-info.java
@@ -2,5 +2,6 @@ import dk.sdu.student.mialb21.common.services.IPostEntityProcessingService;
 
 module Collision {
     requires Common;
+
     provides IPostEntityProcessingService with dk.sdu.student.mialb21.collision.CollisionDetector;
 }
